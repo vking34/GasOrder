72333cd78e2f4a62592b2bb98d6844ed9b818ccc
dungkaka <38502025+dungkaka@users.noreply.github.com>
2018-05-12 11:27:22 +0700

    5/12/2018

--- app/src/main/java/com/dungkk/gasorder/fragment/FragmentOrder.java
+++ app/src/main/java/com/dungkk/gasorder/fragment/FragmentOrder.java
@@ -7,22 +7,75 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.dungkk.gasorder.R;
 import com.dungkk.gasorder.User;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class FragmentOrder extends Fragment{
 
-    TextView name;
+    EditText product, phoneNumber, floor, address, humanCheck;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_order, container, false);
 
-        name = (TextView) view.findViewById(R.id.name);
-        name.setText(User.getUsername());
+        product = (EditText) view.findViewById(R.id.productText);
+        phoneNumber = (EditText) view.findViewById(R.id.phonenumberText);
+        floor = (EditText) view.findViewById(R.id.floorText);
+        address = (EditText) view.findViewById(R.id.addressText);
+
+        JSONObject userJson = new JSONObject();
+        try {
+            userJson.put("product", product.toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        String url = "http://192.168.1.2/profile";
+        RequestQueue requestQueue = Volley.newRequestQueue(getActivity());
+
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, userJson,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Toast.makeText(getActivity(), response.toString(), Toast.LENGTH_LONG).show();
+                        try {
+                            if(response.getBoolean("status")){
+                                product.setText(response.getString("name"));
+                            }
+                            else {
+                                Toast.makeText(getActivity(), "Error", Toast.LENGTH_LONG).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getActivity(), "Error", Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
+
+        requestQueue.add(jsonObjectRequest);
 
         return view;
     }
